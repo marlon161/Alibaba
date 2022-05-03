00d930ce77f7d093345c228baf6d9d16364059e5
marlon161 <104410329+marlon161@users.noreply.github.com>
2022-05-02 22:18:52 -0600

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,1 +1,9 @@
-
+let user = [
+    [100,	'anna0',	'white00',	'Melany Martinez',	    '1325 Candy Rd, San Francisco, CA 96123',	'melmartinez@yahoo.com',	3841019535],
+    [101,	'carol1',	'orange01',	'Marlon Martinez',	'1931 Brown St, Gainesville, FL 85321',	    'marlon_martin@aol.com', 	2548952651],
+    [102,	'julia2',	'yellow02',	'Ingrid Cojulun',	    '1622 Seaside St, Seattle, WA 32569',	    'inCojulun1@msn.com',	    3651469235],
+    [103,	'irene3',	'blue03',	'Irene Everly',	    '1756 East Dr, Houston, TX 28562',	        'irene.everly@gmail.com',	9859423698],
+    [104,	'rachel4',	'red04',	'Nataly Sequén',	    '1465 River Dr, Boston, MA 43625',	        'nataly.seq@hotmail.com',	2945632543],
+    [105,	'sophie5',	'green05',	'Sofia Salazar',	'1896 West Dr, Portland, OR 65842',	        'sgsalazar@yahoo.com',	5169525614],
+    [106,	'wendy6',	'purple06',	'David Martinez',	    '1252 Vine St, Chicago, IL 73215',	        'DaviMart@gmail.com', 	6645936156]
+]
