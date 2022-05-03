601e494af09916860f9d9b1b3aa85df15d804f86
marlon161 <104410329+marlon161@users.noreply.github.com>
2022-05-02 21:58:41 -0600

    Update oders_paid_creditcart.js

--- js/oders_paid_creditcart.js
+++ js/oders_paid_creditcart.js
@@ -1,1 +1,11 @@
+//Base de datos con el numero de tarjeta de crédito y el id del pedido correspondiente.
 
+let OrdersPaid = [
+    [256324,	1000],
+    [135632,	1001],
+    [684323,	1002],
+    [318795,	1003],
+    [356547,	1004],
+    [653214,	1005],
+    [135468,	1006]
+]
