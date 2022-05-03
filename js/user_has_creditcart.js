4003eb370afe162dc0a60fb2b6d39864954a440c
marlon161 <104410329+marlon161@users.noreply.github.com>
2022-05-02 22:20:24 -0600

    Update user_has_creditcart.js

--- js/user_has_creditcart.js
+++ js/user_has_creditcart.js
@@ -1,1 +1,11 @@
+//Base de datos sobre el id del usuario y el número de tarjeta de crédito correspondiente.
 
+let UserCreditcard = [
+    [100,	2.56324]
+    [101,	1.35632]
+    [102,	6.84323]
+    [103,	3.18795]
+    [104,	3.56547]
+    [105,	6.53214]
+    [106,	1.35468]
+]
