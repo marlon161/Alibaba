e26fd5702b5083127188b27e8e218fe6e2b879c2
marlon161 <104410329+marlon161@users.noreply.github.com>
2022-05-02 21:59:20 -0600

    Update oders_placed_user.js

--- js/oders_placed_user.js
+++ js/oders_placed_user.js
@@ -1,1 +1,11 @@
+//Base de datos con el id del usuario y el id del pedido correspondiente.
 
+let OrdersPlaced = [
+    [100,	1000],
+    [101,	1001],
+    [102,	1002],
+    [103,	1003],
+    [104,	1004],
+    [105,	1005],
+    [106,	1006]
+]
