d795bbc46a24f6478d9457e7b2d57f2867428ff7
marlon161 <104410329+marlon161@users.noreply.github.com>
2022-05-02 21:52:40 -0600

    Update category.js

--- js/category.js
+++ js/category.js
@@ -1,1 +1,9 @@
-
+let Category = [
+    [4000,	'Laptops'],
+    [4100,	'Mobile phones'],
+    [4200,	'Tablets'],
+    [4300,	'Servers'],
+    [4400,	'PC'],
+    [4500,	'Cables'],
+    [4600,	'Headphones']
+]
