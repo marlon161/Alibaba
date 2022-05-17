ee29daabc300c37e9a6730edd53e7b8ba1868865
Marlon <totoreto3martinez@gmail.com>
2022-05-17 13:36:55 -0600

    puse una alerta

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,37 @@
+function recuperar_datos() { 
+    var user = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var nombre 
+   
+    if(user=="anna0"){
+        alert('Tu nombre de usuario '+user +' es correcto');
+        if(password=="white00"){
+            alert('tu contraseña es correcta');
+        }else{
+            alert('tu contraseña es incorrecta');
+        }
+    }else if(user=="carol1"){
+        alert('Tu nombre de usuario '+user +' es correcto');
+        if(password=="orange01"){
+            alert('tu contraseña es correcta');
+        }else{
+            alert('tu contraseña es incorrecta');
+        }
+    }else if(user=="julia2"){
+        alert('Tu nombre de usuario '+user +' es correcto');
+        if(password=="yellow02"){
+            alert('tu contraseña es correcta');
+        }else{
+            alert('tu contraseña es incorrecta');
+        }
+    }else{
+        alert('Tu nombre de usuario es incorrecto');
+        nombre=false
+        if(nombre==true){
 
+        }else{
+            alert('Tu contraseña es incorrecta');
+        }
+    }
+    
+}
